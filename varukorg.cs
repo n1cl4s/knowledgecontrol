4e25fcd378c0554a4be92676e54fe6ec46c330d4
n1cl4s <niclas.calabrano@gmail.com>
2026-09-10 13:35:49 +0200

    menushell

--- varukorg.cs
+++ varukorg.cs
@@ -3,4 +3,23 @@ List<int> price = new List<int>();
 
 while (true)
 {
+    System.Console.WriteLine("\nVälj ett alternativ");
+    System.Console.WriteLine("1. Lägg till vara");
+    System.Console.WriteLine("2. Ta bort vara");
+
+    string? menuChoice = Console.ReadLine();
+
+    if (menuChoice == "1")
+    {
+        // Menyval 1: Lägg till vara
+    }
+    else if (menuChoice == "2")
+    {
+        // Menyval 2: Ta bort vara
+    }
+    else
+    {
+        // ERROR 404
+        System.Console.WriteLine("Felaktig inmatning");
+    }
 }
